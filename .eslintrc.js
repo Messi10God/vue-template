90e8d32f0012555e66f644676cbc12235b6b06bd
chen.huang <chen.huang@eclincloud.com>
2023-02-22 17:24:03 +0800

    perf: 修改eslint规则，关闭debugger错误

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
+    'no-debugger': 0,
     'vue/multi-word-component-names': 0,
     "@typescript-eslint/no-explicit-any": [0],
     "@typescript-eslint/no-empty-function": [0],
